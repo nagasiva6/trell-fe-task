12474ad7b661b54d495a5142540225ca77d37d03
Rohith <sairohith1212@gmail.com>
2019-12-15 12:57:37 +0530

    functionality done, todo : css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
+
+const STATUS = {
+  OPEN: "open",
+  CLOSE: "close"
+};
+// let idGen = 0;
+// function newId() {
+//   idGen++;
+//   console.log(idGen);
+//   return idGen;
+// }
+class Display extends React.Component {
+  state = {
+    status: STATUS.CLOSE
+  };
+  toggleDisplay = () => {
+    this.setState(prevState => {
+      if (prevState.status === STATUS.OPEN) {
+        return { status: STATUS.CLOSE };
+      }
+      return { status: STATUS.OPEN };
+    });
+  };
+  render() {
+    const { data, name } = this.props;
+    const { status } = this.state;
+    const childNames = Object.keys(data) || {};
+    return (
+      <div>
+        <p onClick={this.toggleDisplay}>{`${name} -> STATUS : ${status}`}</p>
+        <div>
+          {status === STATUS.OPEN
+            ? childNames.map(ele => {
+                return <Display data={data[ele] || {}} name={ele} key={ele} />;
+              })
+            : null}
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const names = Object.keys(data) || {};
+  const displays = names.map(name => {
+    return <Display name={name} data={data[name] || {}} key={name} />;
+  });
+  return displays.length > 0 ? displays : null;
 }
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,19 @@
+const data = {
+    f1 : {
+        f2 : {
+            f5 : {
+                f6 : {},
+            },
+            f7: {
+                f8: {
+                    f9: {}
+                }
+            },
+            f10:{}
+        },
+        f3 : null,
+    },
+    f4 : {},
+}
+
+export default data;
